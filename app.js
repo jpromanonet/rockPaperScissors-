1a0ee0bb56a2eee2a4b61c02c05ee061b0188437
jpromanonet <programming@jpromano.net>
2019-09-02 22:12:15 -0300

    JS started

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const game = () => {};
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+  };
+};
