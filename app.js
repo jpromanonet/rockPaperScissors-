523426caf8767e6378ba1a727e0d3782548bbf7e
jpromanonet <programming@jpromano.net>
2019-09-02 22:51:18 -0300

    Almost working

--- app.js
+++ app.js
@@ -29,6 +29,10 @@ const game = () => {
         const computerNumber = Math.floor(Math.random() * 3);
         const computerChoice = computerOptions[computerNumber];
         // Here is where we call compare hands
+
+        // Update images
+        playerHand.src = `./assets/${this.textContent}.png`;
+        computerHand.src = `./assets/${computerChoice}.png`;
       });
     });
   };
